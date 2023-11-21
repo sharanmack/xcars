6feb7e7755f29392e7c8297071e8fb42794b4f4c
sharan mack <sharanmack06@hmail.com>
2023-11-21 01:04:25 -1200

    intro/gallery/images//fixed

--- src/app/components/intro/intro.component.ts
+++ src/app/components/intro/intro.component.ts
@@ -21,7 +21,7 @@ export class IntroComponent {
   currentImageUrl = this.images[this.currentImageIndex];
 
   ngOnInit() {
-    setInterval(() => this.changeImage(), 2100); // 2 minutes (2 * 60 * 1000 milliseconds)
+    setInterval(() => this.changeImage(), 2100); 
   }
 
   changeImage() {
